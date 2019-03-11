4604bc3abd5362873d93bdb1b6bd116ae68a3e1a
wu_mingzhu <857110242@qq.com>
2019-03-11 09:45:08 +0800

    git第一次上传

---
+++ podTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  podTest
+//
+//  Created by wumingzhu on 2019/3/8.
+//  Copyright © 2019年 dxrm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
